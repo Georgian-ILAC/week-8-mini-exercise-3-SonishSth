b8fa2a178d21a5649952cade09350a2f7b742924
SonishSth <sonishdshrestha@gmail.com>
2024-07-12 20:26:41 -0400

    Exercise Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee e1 = new Employee(78891, 18, "Sonish", "Shrestha");
+        e1.setHeight(165);
+
+        System.out.println(e1);
+
+        // after employee gets a raise
+        System.out.println("You got a pay raise and your new hourly pay is "+e1.getRaise());
+
+        // printing the weekly income
+        System.out.println("You income this week is "+e1.payDay(45));
     }
 }
